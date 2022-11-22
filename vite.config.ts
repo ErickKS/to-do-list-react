b9f3c46e50c01d1a8d02f7e085c1dc55182462b6
Erick <erick.kuwahara79@gmail.com>
2022-11-22 15:06:26 -0300

    fix: deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: "https://erickks.github.io/to-do-list-react",
+  base: "https://erickks.github.io/to-do-list-react",
 });
